9104f1ea5908040b1118f3d92a6a07eba3077487
Asif <asif.javed.bangash@gmail.com>
2023-04-03 05:51:44 +1000

    authUrl-prod-environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 export const environment = {
   production: true,
   api: {
+    authUrl: 'https://stream-vault-server.herokuapp.com/v1/',
     baseUrl: 'https://stream-vault-server.herokuapp.com/v2/', //URL for API on Production Server
     serverUrl: 'https://stream-vault-server.herokuapp.com/',
     httpOptions: {

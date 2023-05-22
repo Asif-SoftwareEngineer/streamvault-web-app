3293ec010be372146a6c9a7c09ac7f0264a8ef95
Asif <asif.javed.bangash@gmail.com>
2023-05-22 23:10:18 +1000

    membership-in-progress

--- src/app/user/member-registration/member-registration.component.ts
+++ src/app/user/member-registration/member-registration.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-member-registration',
   templateUrl: './member-registration.component.html',
-  styleUrls: ['./member-registration.component.scss']
+  styleUrls: ['./member-registration.component.scss'],
 })
 export class MemberRegistrationComponent implements OnInit {
+  _membershipForm: FormGroup = this._fb.group({});
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  onSubmit(): void {}
 }
